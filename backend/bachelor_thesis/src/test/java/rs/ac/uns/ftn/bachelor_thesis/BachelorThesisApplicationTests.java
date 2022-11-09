b2a48fa9e8413eb5df63cd60365dc6604c837369
andric-milos <andrinho.num7@gmail.com>
2022-11-09 23:40:11 +0100

    Initial back-end application commit

---
+++ backend/bachelor_thesis/src/test/java/rs/ac/uns/ftn/bachelor_thesis/BachelorThesisApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.bachelor_thesis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BachelorThesisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
