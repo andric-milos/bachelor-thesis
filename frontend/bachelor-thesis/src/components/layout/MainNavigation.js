d9aa972074f7287c354decbc18970c9598b70d17
andric-milos <andrinho.num7@gmail.com>
2022-12-21 23:10:05 +0100

    Updated navigation bar to conditionally render its items
    For example: to not show "Logout" button if user isn't logged in.

--- frontend/bachelor-thesis/src/components/layout/MainNavigation.js
+++ frontend/bachelor-thesis/src/components/layout/MainNavigation.js
@@ -1,10 +1,26 @@
-import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 
 import classes from './MainNavigation.module.css';
 
 function MainNavigation() {
+    const [role, setRole] = useState(null);
     const navigate = useNavigate();
+    const location = useLocation();
+
+    useEffect(() => {
+        axios.get("http://localhost:8080/user/whoami", { headers: { 'Authorization': 'Bearer ' + localStorage.getItem("accessToken") } })
+            .then(response => {
+                console.log(response);
+                setRole(response.data.roles[0]);
+            })
+            .catch(error => {
+                console.log(error);
+                setRole(null);
+            });
+    }, [location])
 
     function logout() {
         localStorage.removeItem("accessToken");
@@ -21,24 +37,35 @@ function MainNavigation() {
         <header className={classes.header}>
             <nav>
                 <ul>
-                    <li>
-                        <Link to='/'>Homepage</Link>
-                    </li>
-                    <li>
-                        <Link to='/register'>Register</Link>
-                    </li>
-                    <li>
-                        <Link to='/login'>Login</Link>
-                    </li>
-                    <li>
-                        <Link to='/profile'>Profile</Link>
-                    </li>
-                    <li>
-                        <Link to='/facilities'>Facilities</Link>
-                    </li>
-                    <li>
-                        <button type='button' className='btn btn-dark' onClick={logout}>Logout</button>
-                    </li>
+                    <div className='d-flex flex-row'>
+                        <li>
+                            <Link to='/'>Homepage</Link>
+                        </li>
+                        {role == null ?
+                            <li>
+                                <Link to='/register'>Register</Link>
+                            </li> : null
+                        }
+                        {role == null ?
+                            <li>
+                                <Link to='/login'>Login</Link>
+                            </li> : null
+                        }
+                        {role != null ?
+                            <li>
+                                <Link to='/profile'>Profile</Link>
+                            </li> : null
+                        }
+                        <li>
+                            <Link to='/facilities'>Facilities</Link>
+                        </li>
+                    </div>
+                    {role != null ?
+                        <li>
+                            <button type='button' className='btn btn-dark' onClick={logout}>Logout</button>
+                        </li> : null
+                    }
+
                 </ul>
             </nav>
         </header>
