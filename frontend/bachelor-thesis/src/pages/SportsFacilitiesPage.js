06fb9d6fc9539db79694106fa94f3b714287cc73
milos.andric1 <57445151+andric-milos@users.noreply.github.com>
2023-02-27 23:18:36 +0100

    Displaying message when there are no sports facilities

--- frontend/bachelor-thesis/src/pages/SportsFacilitiesPage.js
+++ frontend/bachelor-thesis/src/pages/SportsFacilitiesPage.js
@@ -17,21 +17,25 @@ function SportsFacilitiesPage() {
 
     return (
         <div className="d-flex flex-row justify-content-center">
-            <div className="d-flex flex-column p-2 col-sm-5">
-                {sportsFacilities.map((facility) => {
-                    return (
-                        <div className="d-flex card my-1" key={`div-card-facility-${facility.id}`}>
-                            <div className="card-body d-flex flex-column" key={`div-cardBody-facility-${facility.id}`}>
-                                <label key={`label1-facility-${facility.id}`} className="h3"><b>{facility.name}</b></label>
-                                <label key={`label2-facility-${facility.id}`}><b>Address: {facility.location.address}</b></label>
-                                <label key={`label3-facility-${facility.id}`}><b>Price per hour: {facility.pricePerHour} RSD</b></label>
-                                <label key={`label4-facility-${facility.id}`}><b>Manager: {facility.managerFirstName} {facility.managerLastName}</b></label>
-                                <label key={`label5-facility-${facility.id}`}><b>Manager's email: {facility.managerEmail}</b></label>
+            {sportsFacilities.length !== 0 ?
+                <div className="d-flex flex-column p-2 col-sm-5">
+                    {sportsFacilities.map((facility) => {
+                        return (
+                            <div className="d-flex card my-1" key={`div-card-facility-${facility.id}`}>
+                                <div className="card-body d-flex flex-column" key={`div-cardBody-facility-${facility.id}`}>
+                                    <label key={`label1-facility-${facility.id}`} className="h3"><b>{facility.name}</b></label>
+                                    <label key={`label2-facility-${facility.id}`}><b>Address: {facility.location.address}</b></label>
+                                    <label key={`label3-facility-${facility.id}`}><b>Price per hour: {facility.pricePerHour} RSD</b></label>
+                                    <label key={`label4-facility-${facility.id}`}><b>Manager: {facility.managerFirstName} {facility.managerLastName}</b></label>
+                                    <label key={`label5-facility-${facility.id}`}><b>Manager's email: {facility.managerEmail}</b></label>
+                                </div>
                             </div>
-                        </div>
-                    )
-                })}
-            </div>
+                        )
+                    })}
+                </div> :
+                <h3 className="m-3">There are no sports facilities registered!</h3>
+            }
+
         </div>
     )
 }
