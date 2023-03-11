2850950fe699539a1d923d89f2b86200547cb212
milos.andric1 <57445151+andric-milos@users.noreply.github.com>
2023-03-11 23:36:21 +0100

    Deleted unnecessary imports

--- backend/bachelor_thesis/src/main/java/rs/ac/uns/ftn/bachelor_thesis/BachelorThesisApplication.java
+++ backend/bachelor_thesis/src/main/java/rs/ac/uns/ftn/bachelor_thesis/BachelorThesisApplication.java
@@ -1,22 +1,14 @@
 package rs.ac.uns.ftn.bachelor_thesis;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import rs.ac.uns.ftn.bachelor_thesis.dto.CreateGroupDTO;
-import rs.ac.uns.ftn.bachelor_thesis.dto.NewAppointmentDTO;
-import rs.ac.uns.ftn.bachelor_thesis.dto.RegisterInfoDTO;
-import rs.ac.uns.ftn.bachelor_thesis.model.*;
 import rs.ac.uns.ftn.bachelor_thesis.security.CustomCorsFilter;
 import rs.ac.uns.ftn.bachelor_thesis.security.TokenUtil;
-import rs.ac.uns.ftn.bachelor_thesis.service.*;
 import rs.ac.uns.ftn.bachelor_thesis.validation.ValidationUtil;
-
-import java.util.*;
 
 @SpringBootApplication
 public class BachelorThesisApplication {
