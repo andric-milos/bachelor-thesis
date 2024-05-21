3ad26eb5a3bc155d2112c9408c01fefd686538c1
andric-milos <57445151+andric-milos@users.noreply.github.com>
2024-05-21 22:26:06 +0200

    Create unit tests for PlayerService class

---
+++ backend/bachelor_thesis/src/test/java/rs/ac/uns/ftn/bachelor_thesis/service/PlayerServiceTest.java
@@ -0,0 +1,38 @@
+package rs.ac.uns.ftn.bachelor_thesis.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import rs.ac.uns.ftn.bachelor_thesis.model.Player;
+import rs.ac.uns.ftn.bachelor_thesis.repository.PlayerRepository;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class PlayerServiceTest {
+    @Mock
+    private PlayerRepository playerRepository;
+    @InjectMocks
+    private PlayerService playerService;
+
+    @Test
+    void getPlayerByEmail_shouldReturnExistingPlayer() {
+        String email = "test@gmail.com";
+        Player player = new Player();
+        player.setEmail(email);
+
+        Mockito.when(playerRepository.findByEmail(email)).thenReturn(Optional.of(player));
+
+        Optional<Player> returnedPlayer = playerService.getPlayerByEmail(email);
+
+        System.out.println(player.getEmail());
+        System.out.println(returnedPlayer.get().getEmail());
+
+        assertEquals(returnedPlayer.get().getEmail(), email);
+    }
+}
